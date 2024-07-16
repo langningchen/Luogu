22ce4eb27ff3c69f29bd30fa4065d95c986eb83b
Langning Chen <chenlangning2009@outlook.com>
2024-07-16 16:51:47 +0800

    Done some

--- P1091.cpp
+++ P1091.cpp
@@ -1,24 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-const ll N = 105;
-ll n, g[N], f[N], a[N], Answer;
+#include <stdio.h>
+int n, g[105], f[105], a[105], Answer;
+#define getchar() (p1 == p2 && (p2 = (p1 = InputBuffer) + fread(InputBuffer, 1, 1 << 20, stdin), p1 == p2) ? EOF : *p1++)
+char InputBuffer[1 << 20], *p1 = InputBuffer, *p2 = InputBuffer;
+int Read() {
+    int Number = 0;
+    short Sign = 1;
+    char c = ' ';
+    while (c < '0' || c > '9') {
+        c = getchar();
+        if (c == '-') Sign = -1;
+    }
+    while (c >= '0' && c <= '9') {
+        Number = Number * 10 + c - '0';
+        c = getchar();
+    }
+    return Number * Sign;
+}
+void Write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) Write(x / 10);
+    putchar(x % 10 + '0');
+}
 int main() {
-    cin >> n;
-    for (ll i = 1; i <= n; i++)
-        cin >> a[i];
-    fill(f + 1, f + n + 1, 1);
-    fill(g + 1, g + n + 1, 1);
-    for (ll i = n - 1; i >= 1; i--)
-        for (ll j = i + 1; j <= n; j++)
+    n = Read();
+    for (int i = 1; i <= n; i++)
+        a[i] = Read();
+    for (int i = 1; i <= n; i++)
+        f[i] = g[i] = 1;
+    for (int i = n - 1; i >= 1; i--)
+        for (int j = i + 1; j <= n; j++)
             if (a[i] > a[j] && f[i] <= f[j] + 1)
                 f[i] = f[j] + 1;
-    for (ll i = 2; i <= n; i++)
-        for (ll j = 1; j < i; j++)
+    for (int i = 2; i <= n; i++)
+        for (int j = 1; j < i; j++)
             if (a[i] > a[j] && g[i] <= g[j] + 1)
                 g[i] = g[j] + 1;
-    for (ll i = 1; i <= n; i++)
-        Answer = max(Answer, f[i] + g[i] - 1);
-    cout << n - Answer << endl;
+    for (int i = 1; i <= n; i++)
+        if (f[i] + g[i] - 1 > Answer)
+            Answer = f[i] + g[i] - 1;
+    Write(n - Answer);
     return 0;
 }
--- P3376.cpp
+++ P3376.cpp
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll N = 5005;
-const int INF = 0x3FFF'FFFF;
+const ll N = 50005;
+const ll INF = 0x3FFF'FFFF'FFFF'FFFF;
 struct GRAPH {
     struct EDGE {
-        int v, Next, cap, flow;
-    } Edge[N];
+        ll To, Next, Capability, Value;
+    } Edges[N];
+    ll Head[N], EdgeCount;
+    ll n, Start, End;
+    ll Depth[N], CurrentHead[N];
+    void AddEdge(ll u, ll v, ll w) {
+        EdgeCount++;
+        Edges[EdgeCount].To = v;
+        Edges[EdgeCount].Next = Head[u];
+        Edges[EdgeCount].Capability = w;
+        Head[u] = EdgeCount;
 
-    int fir[N], EdgeCount = 0;
-
-    int n, Start, End;
-    ll MaxFlow = 0;
-    int Depth[N], CurrentHead[N];
-
-    void init() {
-        memset(fir, -1, sizeof fir);
-        EdgeCount = 0;
+        EdgeCount++;
+        Edges[EdgeCount].To = u;
+        Edges[EdgeCount].Next = Head[v];
+        Edges[EdgeCount].Capability = 0;
+        Head[v] = EdgeCount;
     }
-
-    void addedge(int u, int v, int w) {
-        Edge[EdgeCount] = {v, fir[u], w, 0};
-        fir[u] = EdgeCount++;
-        Edge[EdgeCount] = {u, fir[v], 0, 0};
-        fir[v] = EdgeCount++;
-    }
-
     bool BFS() {
-        queue<int> Queue;
-        memset(Depth, 0, sizeof(int) * (n + 1));
-
+        queue<ll> Queue;
+        memset(Depth, 0, sizeof(Depth));
         Depth[Start] = 1;
         Queue.push(Start);
         while (Queue.size()) {
-            int u = Queue.front();
+            ll u = Queue.front();
             Queue.pop();
-            for (int i = fir[u]; ~i; i = Edge[i].Next) {
-                int v = Edge[i].v;
-                if (!Depth[v] && Edge[i].cap > Edge[i].flow) {
+            for (ll i = Head[u]; i; i = Edges[i].Next) {
+                ll v = Edges[i].To;
+                if (!Depth[v] && Edges[i].Capability > Edges[i].Value) {
                     Depth[v] = Depth[u] + 1;
                     Queue.push(v);
                 }
@@ -45,42 +41,39 @@ struct GRAPH {
         }
         return Depth[End];
     }
-
-    int DFS(int u, int flow) {
-        if (u == End || !flow) return flow;
-
-        int Return = 0;
-        for (int &i = CurrentHead[u]; ~i; i = Edge[i].Next) {
-            int v = Edge[i].v, d;
-            if ((Depth[v] == Depth[u] + 1) &&
-                (d = DFS(v, min(flow - Return, Edge[i].cap - Edge[i].flow)))) {
-                Return += d;
-                Edge[i].flow += d;
-                Edge[i ^ 1].flow -= d;
-                if (Return == flow) return Return;
-            }
+    ll DFS(ll u, ll Value) {
+        if (u == End || !Value) return Value;
+        ll Answer = 0;
+        for (ll &i = CurrentHead[u]; i; i = Edges[i].Next) {
+            ll v = Edges[i].To, d;
+            if (Depth[v] != Depth[u] + 1) continue;
+            d = DFS(v, min(Value - Answer, Edges[i].Capability - Edges[i].Value));
+            if (!d) continue;
+            Answer += d;
+            Edges[i].Value += d;
+            Edges[i % 2 == 0 ? i - 1 : i + 1].Value -= d;
+            if (Answer == Value) return Answer;
         }
-        return Return;
+        return Answer;
     }
 
-    void dinic() {
+    ll Dinic() {
+        ll Answer = 0;
         while (BFS()) {
-            memcpy(CurrentHead, fir, sizeof(int) * (n + 1));
-            MaxFlow += DFS(Start, INF);
+            memcpy(CurrentHead, Head, sizeof(Head));
+            Answer += DFS(Start, INF);
         }
+        return Answer;
     }
-} mf;
-
+} Graph;
 int main() {
-    int m;
-    cin >> mf.n >> m >> mf.Start >> mf.End;
-    mf.init();
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
+    ll m;
+    cin >> Graph.n >> m >> Graph.Start >> Graph.End;
+    for (ll i = 0; i < m; i++) {
+        ll u, v, w;
         cin >> u >> v >> w;
-        mf.addedge(u, v, w);
+        Graph.AddEdge(u, v, w);
     }
-    mf.dinic();
-    cout << mf.MaxFlow << endl;
+    cout << Graph.Dinic() << endl;
     return 0;
 }
