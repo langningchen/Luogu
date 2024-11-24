3d97ae692b9298f81e6ddb4c42ad637ec4c8bf74
Langning Chen <chenlangning2009@outlook.com>
2024-11-24 22:08:30 +0800

    Finish P10526

--- P10526.cpp
+++ P10526.cpp
@@ -4,8 +4,7 @@ const int N = 1e5 + 5;
 const int K = 26;
 int n, m;
 
-// Trie
-int fa[N], son[N][K], sonCnt[N], cnt, idx[N];
+int fa[N], son[N][K], sonCnt[N], oneSon[N], cnt, idx[N], tab[N];
 void add(string s, int i)
 {
     int p = 0;
@@ -14,7 +13,12 @@ void add(string s, int i)
         int u = s[i] - 'a';
         if (!son[p][u])
         {
-            sonCnt[p]++;
+            // sonCnt[p]++;
+            // oneSon[p] = u;
+            if (tab[p])
+                tab[p] = -1;
+            else if (!tab[p])
+                tab[p] = u;
             son[p][u] = ++cnt;
             fa[cnt] = p;
         }
@@ -23,51 +27,57 @@ void add(string s, int i)
     idx[p] = i;
 }
 
+int getTab(int x)
+{
+    if (sonCnt[x] != 1 || idx[x] != 0)
+        return x;
+    if (tab[x])
+        return tab[x];
+    return tab[x] = getTab(son[x][oneSon[x]]);
+}
+
 int main()
 {
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         string t;
         cin >> t;
-        add(t, i + 1);
+        add(t, i);
     }
     string p;
     cin >> p;
-    int cur = 0;
-    string buff;
+    int cur = 0, more = 0;
     for (int i = 0; i < m; i++)
     {
         if (p[i] == 'T')
         {
-            if (buff.size() == 0)
-                while (sonCnt[cur] == 1)
-                    cur = son[cur][0];
+            if (!more)
+                cur = getTab(cur);
         }
         else if (p[i] == 'B')
         {
-            if (buff.size())
-                buff.pop_back();
-            else if (cur != 1)
+            if (more)
+                more--;
+            else if (cur != 0)
                 cur = fa[cur];
         }
         else if (p[i] == 'E')
         {
-            if (!idx[cur] == 0 || buff.size())
+            if (!idx[cur] || more)
                 cout << -1 << " ";
             else
                 cout << idx[cur] << " ";
-            cur = 0;
-            buff = "";
+            cur = more = 0;
         }
         else
         {
             if (son[cur][p[i] - 'a'])
                 cur = son[cur][p[i] - 'a'];
             else
-                buff.push_back(p[i]);
+                more++;
         }
-        cerr << cur << " " << buff << endl;
+        // cerr << cur << " \"" << buff << "\"" << endl;
     }
     return 0;
 }
